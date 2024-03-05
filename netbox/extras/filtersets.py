ebd499f3160ca703dfe6d009fe142bbdc32f3c28
Jeremy Stretch <jstretch@netboxlabs.com>
2024-03-05 17:15:21 -0500

    Add missing filters

--- netbox/extras/filtersets.py
+++ netbox/extras/filtersets.py
@@ -40,12 +40,14 @@ class ScriptFilterSet(BaseFilterSet):
         method='search',
         label=_('Search'),
     )
+    module_id = django_filters.ModelMultipleChoiceFilter(
+        queryset=ScriptModule.objects.all(),
+        label=_('Script module (ID)'),
+    )
 
     class Meta:
         model = Script
-        fields = [
-            'id', 'name',
-        ]
+        fields = ('id', 'name', 'is_executable')
 
     def search(self, queryset, name, value):
         if not value.strip():
@@ -69,10 +71,10 @@ class WebhookFilterSet(NetBoxModelFilterSet):
 
     class Meta:
         model = Webhook
-        fields = [
+        fields = (
             'id', 'name', 'payload_url', 'http_method', 'http_content_type', 'secret', 'ssl_verification',
             'ca_file_path', 'description',
-        ]
+        )
 
     def search(self, queryset, name, value):
         if not value.strip():
@@ -101,10 +103,10 @@ class EventRuleFilterSet(NetBoxModelFilterSet):
 
     class Meta:
         model = EventRule
-        fields = [
+        fields = (
             'id', 'name', 'type_create', 'type_update', 'type_delete', 'type_job_start', 'type_job_end', 'enabled',
             'action_type', 'description',
-        ]
+        )
 
     def search(self, queryset, name, value):
         if not value.strip():
@@ -116,7 +118,7 @@ class EventRuleFilterSet(NetBoxModelFilterSet):
         )
 
 
-class CustomFieldFilterSet(BaseFilterSet):
+class CustomFieldFilterSet(ChangeLoggedModelFilterSet):
     q = django_filters.CharFilter(
         method='search',
         label=_('Search'),
@@ -130,6 +132,7 @@ class CustomFieldFilterSet(BaseFilterSet):
     object_types = ContentTypeFilter(
         field_name='object_types'
     )
+    object_type = ContentTypeFilter()
     choice_set_id = django_filters.ModelMultipleChoiceFilter(
         queryset=CustomFieldChoiceSet.objects.all()
     )
@@ -141,10 +144,11 @@ class CustomFieldFilterSet(BaseFilterSet):
 
     class Meta:
         model = CustomField
-        fields = [
-            'id', 'object_types', 'name', 'group_name', 'required', 'search_weight', 'filter_logic', 'ui_visible',
-            'ui_editable', 'weight', 'is_cloneable', 'description',
-        ]
+        fields = (
+            'id', 'object_types', 'name', 'label', 'group_name', 'required', 'search_weight', 'filter_logic',
+            'ui_visible', 'ui_editable', 'weight', 'is_cloneable', 'description', 'validation_minimum',
+            'validation_maximum', 'validation_regex',
+        )
 
     def search(self, queryset, name, value):
         if not value.strip():
@@ -157,7 +161,7 @@ class CustomFieldFilterSet(BaseFilterSet):
         )
 
 
-class CustomFieldChoiceSetFilterSet(BaseFilterSet):
+class CustomFieldChoiceSetFilterSet(ChangeLoggedModelFilterSet):
     q = django_filters.CharFilter(
         method='search',
         label=_('Search'),
@@ -168,9 +172,9 @@ class CustomFieldChoiceSetFilterSet(BaseFilterSet):
 
     class Meta:
         model = CustomFieldChoiceSet
-        fields = [
+        fields = (
             'id', 'name', 'description', 'base_choices', 'order_alphabetically',
-        ]
+        )
 
     def search(self, queryset, name, value):
         if not value.strip():
@@ -185,7 +189,7 @@ class CustomFieldChoiceSetFilterSet(BaseFilterSet):
         return queryset.filter(extra_choices__overlap=value)
 
 
-class CustomLinkFilterSet(BaseFilterSet):
+class CustomLinkFilterSet(ChangeLoggedModelFilterSet):
     q = django_filters.CharFilter(
         method='search',
         label=_('Search'),
@@ -197,9 +201,10 @@ class CustomLinkFilterSet(BaseFilterSet):
 
     class Meta:
         model = CustomLink
-        fields = [
+        fields = (
             'id', 'object_types', 'name', 'enabled', 'link_text', 'link_url', 'weight', 'group_name', 'new_window',
-        ]
+            'button_class',
+        )
 
     def search(self, queryset, name, value):
         if not value.strip():
@@ -212,7 +217,7 @@ class CustomLinkFilterSet(BaseFilterSet):
         )
 
 
-class ExportTemplateFilterSet(BaseFilterSet):
+class ExportTemplateFilterSet(ChangeLoggedModelFilterSet):
     q = django_filters.CharFilter(
         method='search',
         label=_('Search'),
@@ -232,7 +237,10 @@ class ExportTemplateFilterSet(BaseFilterSet):
 
     class Meta:
         model = ExportTemplate
-        fields = ['id', 'object_types', 'name', 'description', 'data_synced']
+        fields = (
+            'id', 'object_types', 'name', 'description', 'mime_type', 'file_extension', 'as_attachment',
+            'auto_sync_enabled', 'data_synced',
+        )
 
     def search(self, queryset, name, value):
         if not value.strip():
@@ -243,7 +251,7 @@ class ExportTemplateFilterSet(BaseFilterSet):
         )
 
 
-class SavedFilterFilterSet(BaseFilterSet):
+class SavedFilterFilterSet(ChangeLoggedModelFilterSet):
     q = django_filters.CharFilter(
         method='search',
         label=_('Search'),
@@ -268,7 +276,7 @@ class SavedFilterFilterSet(BaseFilterSet):
 
     class Meta:
         model = SavedFilter
-        fields = ['id', 'object_types', 'name', 'slug', 'description', 'enabled', 'shared', 'weight']
+        fields = ('id', 'object_types', 'name', 'slug', 'description', 'enabled', 'shared', 'weight')
 
     def search(self, queryset, name, value):
         if not value.strip():
@@ -312,17 +320,16 @@ class BookmarkFilterSet(BaseFilterSet):
         fields = ['id', 'object_id']
 
 
-class ImageAttachmentFilterSet(BaseFilterSet):
+class ImageAttachmentFilterSet(ChangeLoggedModelFilterSet):
     q = django_filters.CharFilter(
         method='search',
         label=_('Search'),
     )
-    created = django_filters.DateTimeFilter()
     object_type = ContentTypeFilter()
 
     class Meta:
         model = ImageAttachment
-        fields = ['id', 'object_type_id', 'object_id', 'name']
+        fields = ('id', 'object_type_id', 'object_id', 'name', 'image_width', 'image_height')
 
     def search(self, queryset, name, value):
         if not value.strip():
@@ -567,7 +574,7 @@ class ConfigContextFilterSet(ChangeLoggedModelFilterSet):
 
     class Meta:
         model = ConfigContext
-        fields = ['id', 'name', 'is_active', 'data_synced', 'description']
+        fields = ('id', 'name', 'is_active', 'description', 'weight', 'auto_sync_enabled', 'data_synced')
 
     def search(self, queryset, name, value):
         if not value.strip():
@@ -579,7 +586,7 @@ class ConfigContextFilterSet(ChangeLoggedModelFilterSet):
         )
 
 
-class ConfigTemplateFilterSet(BaseFilterSet):
+class ConfigTemplateFilterSet(ChangeLoggedModelFilterSet):
     q = django_filters.CharFilter(
         method='search',
         label=_('Search'),
@@ -596,7 +603,7 @@ class ConfigTemplateFilterSet(BaseFilterSet):
 
     class Meta:
         model = ConfigTemplate
-        fields = ['id', 'name', 'description', 'data_synced']
+        fields = ('id', 'name', 'description', 'auto_sync_enabled', 'data_synced')
 
     def search(self, queryset, name, value):
         if not value.strip():
@@ -644,10 +651,10 @@ class ObjectChangeFilterSet(BaseFilterSet):
 
     class Meta:
         model = ObjectChange
-        fields = [
+        fields = (
             'id', 'user', 'user_name', 'request_id', 'action', 'changed_object_type_id', 'changed_object_id',
-            'object_repr',
-        ]
+            'related_object_type', 'related_object_id', 'object_repr',
+        )
 
     def search(self, queryset, name, value):
         if not value.strip():
